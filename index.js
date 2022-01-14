ca68bdfa6021c75f79b08af7ea3d2c2cee852b6c
Samuel Nascimento <sfsnascimento@gmail.com>
2022-01-14 15:24:21 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -22,6 +22,18 @@ app.get('/talker', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await fs.readFile('./talker.json', 'utf-8')
+    .then((response) => JSON.parse(response));
+  
+  const talker = talkers.find((t) => t.id === parseInt(id, 10));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talker);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
